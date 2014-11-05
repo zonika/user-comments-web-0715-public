2860ac03a2da249e7c75e987fd014cc42d8f4f85
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-11-05 12:30:01 -0500

    fixes test to make more sense

--- spec/comments-spec.js
+++ spec/comments-spec.js
@@ -2,33 +2,40 @@ describe('Validating User Input', function() {
   beforeEach(function() {
     setFixtures('<section id="comments"><h1>User Comments</h1><div id="posts"><div class="newcomment"><span class="name">Lisa Gould</span><span class="email">lisa.gould7684@ymail.com</span><span class="date">09/03/2012</span><p class="comment">Great reading! Thank you Econ News... I would like to see more articles from Chef Boullion. I\'m enjoying the keylime pie recipe with friends.</p></div></div><button id="show-comment" onclick="#add-comment">add comment</button><form id="add-comment" action="#" method="post"><label for="comment-name">Name</label><br><input type="text" id="comment-name" name="comment-name"><br><span id="com-name-error">Name must be more than 3 characters!</span><br><label for="com-email">Email</label><br><input type="text" id="com-email" name="com-email"><br><span id="com-email-error">Must be a valid email!</span><br><label for="comment-name">Comment</label><br><textarea id="comment" name="comment" rows="5"></textarea><br><span id="comment-error">Comment text required!</span><br><input type="submit" value="submit"><input id="cancel" type="reset" value="cancel"></form></section>');
     $('form').submit(function(e){ e.preventDefault(); });
-    hideErrors();
-    hideForm();
-    addCommentListener();
-    cancelListener();
-    submitCommentListener();
   });
 
   it('#hideErrors should hide all errors when the page loads', function() {
+    hideErrors();
     expect($('#com-name-error').css('display')).toBe('none');
     expect($('#com-email-error').css('display')).toBe('none');
     expect($('#comment-error').css('display')).toBe('none');
   });
 
   it('#hideForm should also hide the form until you click add comment', function() {
+    hideForm();
     expect($('form#add-comment').css('display')).toBe('none');
   });
 
   it('#addCommentListener should display the form when you click add comment', function() {
+    addCommentListener();
     expect($('form#add-comment').css('display')).not.toBe('none');
   });
 
   it('#cancelListener should hide form when cancel is pressed', function() {
+    cancelListener();
     $('#cancel').click();
     expect($('form#add-comment').css('display')).toBe('none');
   });
 
   describe('#submitCommentListener', function() {
+    beforeEach(function(){
+      hideErrors();
+      hideForm();
+      addCommentListener();
+      cancelListener();
+      submitCommentListener();
+    });
+
     describe('Displays errors with invalid inputs', function() {
       it('#nameValidator should display the name error with a name less than 3 characters', function() {
         $('#comment-name').text('t');
